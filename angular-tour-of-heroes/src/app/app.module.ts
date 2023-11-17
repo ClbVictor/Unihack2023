9917c5ba84c82c2d652cd59535d3b8d6a22762ac
ClbVictor <calbureanuvictor@gmail.com>
2023-11-18 00:06:20 +0200

    asdasdasda
    asdasdasd

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaworkoutComponent } from './paginaworkout/paginaworkout.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { ChatbotComponent } from './chatbot/chatbot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PaginaworkoutComponent,
+    DoctorComponent,
+    ChatbotComponent,
   ],
   imports: [
     BrowserModule,
---
+++ angular-tour-of-heroes/src/app/paginaworkout/paginaworkout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paginaworkout',
+  templateUrl: './paginaworkout.component.html',
+  styleUrls: ['./paginaworkout.component.css']
+})
+export class PaginaworkoutComponent {
+
+}
