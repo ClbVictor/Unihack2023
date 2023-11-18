e9a494d94d9dbc8a827a3f9164212418befb9946
ClbVictor <calbureanuvictor@gmail.com>
2023-11-18 23:32:46 +0200

    xczxc
    asdasdad

--- aido2/src/app/home/home.component.ts
+++ aido2/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private renderer: Renderer2, private elRef: ElementRef) {}
 
+  ngOnInit() {
+    const teamButton = this.elRef.nativeElement.querySelector('#teamButton');
+    const appDescription = this.elRef.nativeElement.querySelector('.app-description');
+    const teamDescription = this.elRef.nativeElement.querySelector('.team-description');
+
+    teamButton.addEventListener('click', () => {
+      if (appDescription.classList.contains('hidden')) {
+        this.renderer.removeClass(appDescription, 'hidden');
+        this.renderer.addClass(teamDescription, 'hidden');
+      } else {
+        this.renderer.addClass(appDescription, 'hidden');
+        this.renderer.removeClass(teamDescription, 'hidden');
+      }
+    });
+  }
 }
